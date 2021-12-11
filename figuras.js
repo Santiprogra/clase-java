b290f50591f08ca3a4803ca64b027bb7d0f5421d
Santiprogra <santiagobrinez2003@gmail.com>
2021-12-11 01:09:18 -0500

    calcular area del cuadrado

--- figuras.js
+++ figuras.js
@@ -1,1 +1,8 @@
-console.log("Hello world")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " +ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " +perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado ;
+console.log("El perimetro del cuadrado es: " +areaCuadrado);
